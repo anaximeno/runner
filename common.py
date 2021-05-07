5b2de5be3551edceec8c30e7ff9cdb313eb1d5d9
anaximeno <anaximenobrito@gmail.com>
2021-05-07 22:52:33 -0100

    now it can send args to the execution

--- common.py
+++ common.py
@@ -1,3 +1,7 @@
+"""
+Nothing was made yet here!
+"""
+
 import argparse
 import sys
 import os
@@ -5,5 +9,5 @@ import os
 # Support module for the program
 __author__ = 'Anaxímeno Brito'
 __copyright__ = 'Copyright (c) 2021 by ' + __author__
-__version__ = '0.1-pre-alpha'
+__version__ = '0.3-pre-alpha'
 __license__ = 'undefined already'
--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """
 TODO: I want to make  program that compiles and executes different types of programming languages.
-
 NOTE: The name of the program is undefined yet, but the main options are 'execute', 'run' (or another :)
+
 NOTE: I already don't know how to recognize errors when trying to execute the command,
     but I believe that is possible to use ML to do that work (concretely: NLP).
+NOTE: Another use of NLP is to predict the language used if no extension were given.
 """
 import argparse
 import sys
@@ -14,7 +15,7 @@ from os import path
 
 __author__ = 'Anaxímeno Brito'
 __copyright__ = 'Copyright (c) 2021 by ' + __author__
-__version__ = '0.2-pre-alpha'
+__version__ = '0.3-pre-alpha'
 __license__ = 'undefined already'
 
 
@@ -25,18 +26,17 @@ def print_error(*errors, to_exit: bool = False):
         exit(1)
 
 
-class File:
+class File(object):
 
     def __init__(self, filename: str):
         self._exists = path.exists(filename)
         if '.' in filename:
             self._name, self._extension = path.splitext(filename)
         else:
-            self._name, self._extension = filename, ''
-                
+            self._name, self._extension = filename, ''           
 
     def __str__(self):
-        return self.get_name(self.get_extension())
+        return self.get_fullname()
 
     def get_name(self, ext: str = '') -> str:
         return str(self._name) + str(ext)
@@ -56,23 +56,30 @@ class Runner(object):
     def __init__(self, args):
         self.args = args
         self.file = File(args.filename[0])
+        self.command_args = ' '.join(args.args) if args.args else ''
         if self.file.existence() is False:
             print_error(f'{self.file.get_fullname()!r} was not found!', to_exit=True)
 
     def execute(self):
         if self.args.python:
-            os.system(f'/usr/bin/python3 {self.file.get_fullname()}')
+            os.system(
+                f'/usr/bin/python3 {self.file.get_fullname()} {self.command_args}'
+            )
         elif self.args.clang:
-            output = self.file.get_name(ext='.tmp.out')
+            output = self.file.get_name(
+                ext='.tmp.out' if self.args.keep_compiled is not True else ''
+            )
             os.system(f'gcc {self.file.get_fullname()} -o {output}')
             if path.exists(output):
-                os.system(f'./{output}')
+                os.system(f'./{output} {self.command_args}')
                 # NOTE: Below should be an argparse arguments to keep or eliminate compiled files.
-                if True:
+                if self.args.keep_compiled is not True:
                     os.system(f'rm {output}')
-        elif self.args.cplusplus:
-            print("This part isn't finished!")
+        # TODO: finish the cpp compiling process
+        #elif self.args.cplusplus:
+        #    print("This part isn't finished!")
         else:
+            # TODO: try to predict the language and then do the procedure
             print_error('The programming language was not chosen!', to_exit=True)
 
 
@@ -89,10 +96,18 @@ if __name__ == '__main__':
     )
 
     parser.add_argument('filename', nargs=1, help='name of the file.')
+
+    # Exclusive arguments
     group = parser.add_mutually_exclusive_group()
     group.add_argument('-c', '--clang', action='store_true')
     group.add_argument('-py', '--python', action='store_true')
-    group.add_argument('-cpp', '--cplusplus', action='store_true')
+    #group.add_argument('-cpp', '--cplusplus', action='store_true')
+
+    parser.add_argument('-a', '--args', help='arguments to the execution', nargs='+')
+    parser.add_argument('--keep-compiled', 
+        help='keep compiled file on compiled languages',
+        action='store_true'
+    )
 
     runner = Runner(parser.parse_args())
     runner.execute()
